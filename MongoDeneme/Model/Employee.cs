ab7c4e882f4af1db9100713a7f5c7ef146634e93
turgut.karadag <turgut.karadag@zanhagrup.com>
2020-09-29 19:18:32 +0300

    initial

---
+++ MongoDeneme/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDeneme.Model
+{
+    public class Employee
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ MongoDeneme/Model/EmployeeDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace MongoDeneme.Model
+{
+    public class EmployeeDatabaseSettings
+    {
+        public string EmployeesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IEmployeeDatabaseSettings
+    {
+        public string EmployeesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
